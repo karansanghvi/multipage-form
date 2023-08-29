ea1f51e660d506c222e0e5b29adeef0605891c33
Karan Sanghvi <111078325+karansanghvi@users.noreply.github.com>
2023-08-29 15:48:40 +0530

    multipage form done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
+import { useState } from 'react';
 import './App.css';
 import Stepper from './components/Stepper';
 import StepperControl from './components/StepperControl';
+import Account from './components/steps/Account';
+import Details from './components/steps/Details';
+import Final from './components/steps/Final';
+import { StepperContext } from './components/contexts/StepperContext';
 
 function App() {
+
+  const [currentStep, setCurrentStep] = useState(1);
+  const [userData, setUserData] = useState('');
+  const [finalData, setFinalData] = useState([]);
+
+  const steps = [
+    "Account Information",
+    "Personal Details",
+    "Complete"
+  ];
+
+  const displayStep = (step) => {
+    switch(step) {
+      case 1:
+        return <Account/>
+      case 2:
+        return <Details/>
+      case 3:
+        return <Final/>
+      default:
+    }
+  }
+
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+    direction === "next" ? newStep++ : newStep--;
+    // check if steps are within bounds
+    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+  }
+
   return (
     <>
       <div className="md: w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
         {/* stepper */}
           <div className="container horizontal mt-5">
-            <Stepper/>
+            <Stepper
+              steps = {steps}
+              currentStep = {currentStep}
+            />
+          
+          {/* display components */}
+          <div className='my-10 p-10'>
+            <StepperContext.Provider value={{
+              userData,
+              setUserData,
+              finalData,
+              setFinalData
+            }}>
+              {displayStep(currentStep)}
+            </StepperContext.Provider>
           </div>
+
+          </div>
+
         {/* navigation control */}
-          <StepperControl/>
+          <StepperControl
+            handleClick = {handleClick}
+            currentStep = {currentStep}
+            steps = {steps}
+          />
       </div>
     </>
   );
